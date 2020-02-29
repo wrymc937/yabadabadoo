77e28e66392a7fec91bd97d79447017dc37362ba
wrymc937 <gheee951@gmail.com>
2020-02-29 18:22:18 +0100

    add CmakLists.txt and main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Program rysujacy kreske" << endl;
+    int i = 0;
+    for(i=0; i<999999; i++){
+        cout << "_";
+    }
+    return 0;
+}
